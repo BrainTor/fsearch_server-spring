9137fc2c943fdf37504c598d61a7c59643778bca
Maxim Yacenko <m.yacenko@gmail.com>
2016-05-29 13:31:14 +0300

    first commit

---
+++ src/main/java/com/fsearch/Coordinates.java
@@ -0,0 +1,16 @@
+package com.fsearch;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Coordinates extends HttpServlet {
+	@RequestMapping("/")
+	public String index(HttpServletRequest request, HttpServletResponse response) {
+		return "hello world";
+	}
+}
---
+++ src/main/java/com/fsearch/FseachserverApplication.java
@@ -0,0 +1,12 @@
+package com.fsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FseachserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FseachserverApplication.class, args);
+	}
+}
