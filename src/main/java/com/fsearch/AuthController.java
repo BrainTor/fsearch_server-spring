a3aef3ddf8fc076eaa1de96b82182add8843a995
Maxim Yacenko <m.yacenko@gmail.com>
2016-06-09 10:48:15 +0300

    fix bugs

--- src/main/java/com/fsearch/AuthController.java
+++ src/main/java/com/fsearch/AuthController.java
@@ -3,6 +3,7 @@ package com.fsearch;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,30 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-	ArrayList<Drone> droneList = new ArrayList<Drone>();
-	ArrayList<Client> clientList = new ArrayList<Client>();
-	@RequestMapping(value="/auth/fillDrone",method={RequestMethod.POST,RequestMethod.GET})
-	public Boolean setDrone(
-			@RequestParam(value = "hashName", required = true) String hashName,
-			@RequestParam(value = "hashNameDrone", required = true) String hashNameDrone)
-		    {
-		if(!hashName.equals("password")){
+	@Autowired
+	private DroneRepository droneRepository;
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@RequestMapping(value = "/auth/fillDrone", method = { RequestMethod.POST, RequestMethod.GET })
+	public Boolean setDrone(@RequestParam(value = "hashName", required = true) String hashName,
+			@RequestParam(value = "hashNameDrone", required = true) String hashNameDrone) {
+		if (!hashName.equals("password")) {
 			return false;
 		}
-		Drone drone = new Drone(0,hashNameDrone);
-		droneList.add(drone);
+		Drone drone = new Drone(0, hashNameDrone);
+		droneRepository.createDrone(drone);
 		return true;
 	}
-	@RequestMapping(value="/auth/fillClient",method={RequestMethod.POST,RequestMethod.GET})
-	public Boolean setClient(
-			@RequestParam(value = "hashName", required = true) String hashName,
-			@RequestParam(value = "hashNameClient", required = true) String hashNameClient)
-		    {
-		if(!hashName.equals("password")){
+
+	@RequestMapping(value = "/auth/fillClient", method = { RequestMethod.POST, RequestMethod.GET })
+	public Boolean setClient(@RequestParam(value = "hashName", required = true) String hashName,
+			@RequestParam(value = "hashNameClient", required = true) String hashNameClient) {
+		if (!hashName.equals("password")) {
 			return false;
 		}
-		Client client = new Client(0,hashNameClient);
-		clientList.add(client);
+		Client client = new Client(0, hashNameClient);
+		clientRepository.createClient(client);
 		return true;
 	}
 
--- src/main/java/com/fsearch/DroneRepository.java
+++ src/main/java/com/fsearch/DroneRepository.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Component;
 public class DroneRepository {
 @Autowired
 private JdbcTemplate jdbcTemplate;
-public int createFire(Drone drone){
+public int createDrone(Drone drone){
 return	jdbcTemplate.update("INSERT INTO \"DRONE\" (\"hashName\") VALUES (?)",drone.getHashName() );
 }
-public List<Drone> getFire(){
+public List<Drone> getDrone(){
 return jdbcTemplate.query("SELECT * FROM 	\"DRONE\"", 	new	DroneMapper());
 }
 }
