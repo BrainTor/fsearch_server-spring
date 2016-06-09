ab53d21059279d69bf91e663dc78f0b1bb6273d5
Maxim Yacenko <m.yacenko@gmail.com>
2016-06-09 13:38:28 +0300

    Fix bugs

--- src/main/java/com/fsearch/FireController.java
+++ src/main/java/com/fsearch/FireController.java
@@ -5,14 +5,13 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 @RestController
 public class FireController {
